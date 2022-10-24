6448749af72133d4eaf837d3b59a1f339a4f6062
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-24 13:04:54 +0600

    data added and route to localhost server

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 const port = process.env.PORT || 5000
+
+const category = require(`./data/category.json`)
+const products = require(`./data/products.json`)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/category', (req, res) => {
+    res.send(category)
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const productPerCategory = products.filter(perProduct => perProduct._id === id) || {}
+    res.send(productPerCategory)
 })
 
 app.listen(port, () => {
